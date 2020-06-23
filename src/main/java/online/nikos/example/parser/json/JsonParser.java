f5f17d9fd129b5e3707213eb33719e6b92f087d2
Nikos Gioulos <n.gioulos@afse.eu>
2020-06-23 11:51:56 +0300

    code beautification

--- src/main/java/online/nikos/example/parser/json/JsonParser.java
+++ src/main/java/online/nikos/example/parser/json/JsonParser.java
@@ -20,14 +20,17 @@ public class JsonParser {
         this.i=0;
 
         int depth = -1;
+        int MAX_DEPTH = 100;
 
         int arrayIndex=0;
 
+        //it indicates whether we are in array or not
         boolean arrayPending = false;
 
+        //it indicates if an array contains primitive or object items
         boolean arrayPendingObj = false;
 
-        String[] strNames = new String[100];
+        String[] strNames = new String[MAX_DEPTH];
 
         StringBuffer currentString = new StringBuffer();
 
@@ -41,21 +44,16 @@ public class JsonParser {
             char c = this.str.charAt(i);
 
             if(c=='{'){ //start object event
-                depth += 1;
-                if(depth==0) {
-                    strNames[depth] = "";
-                }else{
-                    strNames[depth] =  strNames[depth-1];
-                }
+                strNames[++depth] = depth==0 ? "" : strNames[depth-1];
             } else if(c=='}'){//end object event
                 myData(strNames[depth],currentString.toString());
                 currentString.setLength(0); //clear
                 depth -= 1;
                 arrayPendingObj = false;
             } else if(c==','){//end value event, start key event
-                if(!arrayPending) {
                 myData(strNames[depth],currentString.toString());
                 currentString.setLength(0); //clear
+                if(!arrayPending) { //we are NOT within array
                     int lastSlash = strNames[depth].lastIndexOf("/");
                     strNames[depth] = strNames[depth].substring(0, lastSlash);
                 } else if(arrayPendingObj){ //we are within an array of object items
